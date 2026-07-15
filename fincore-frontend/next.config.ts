9177dc28e10b7347cc63a7dccd2c23b8d69ccba2
trongbinf <trongofficel@gmail.com>
2026-07-16 01:57:18 +0700

    chore: setup Next.js API environment configurations

--- fincore-frontend/next.config.ts
+++ fincore-frontend/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || "https://fincore-c6gugketdtd5fycz.eastasia-01.azurewebsites.net/api",
+  },
 };
 
 export default nextConfig;
